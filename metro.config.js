accb7d1b63611c7b77b96c2897b1a7f6b28bc984
natively.dev <api@natively.dev>
2025-11-17 00:04:07 +0000

    fix: Resolve dependency conflicts and build issues

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,4 @@
+
 const { getDefaultConfig } = require('expo/metro-config');
 const { FileStore } = require('metro-cache');
 const path = require('path');
@@ -6,7 +7,13 @@ const config = getDefaultConfig(__dirname);
 
 // Use turborepo to restore the cache when possible
 config.cacheStores = [
-    new FileStore({ root: path.join(__dirname, 'node_modules', '.cache', 'metro') }),
-  ];
+  new FileStore({ root: path.join(__dirname, 'node_modules', '.cache', 'metro') }),
+];
+
+// Enable package exports resolution
+config.resolver = {
+  ...config.resolver,
+  unstable_enablePackageExports: true,
+};
 
 module.exports = config;
